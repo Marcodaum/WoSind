88f80bce16ff849a021dcea122576e7a7a4ab05b
Marco <marcodaum2@gmail.com>
2021-12-02 23:01:41 +0100

    Poolbau

---
+++ wo_sind_app/lib/GUI/mainColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class mainColors {
+  static Color main_btn = Color.fromRGBO(1, 139, 130, 1);
+  static Color heart_btn = Color.fromRGBO(140, 195, 155, 1);
+  static Color background = Color.fromRGBO(240, 240, 240, 1);
+  static Color Text = Color.fromRGBO(0, 0, 0, 1);
+  static Color Text_description = Color.fromRGBO(120, 120, 120, 1);
+}
--- wo_sind_app/lib/pageContent/productPage.dart
+++ wo_sind_app/lib/pageContent/productPage.dart
@@ -3,67 +3,176 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
 import 'imageList.dart';
+import '../GUI/mainColors.dart';
 
 class ProductPage extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Expanded(
-            child: imageList(),
+        body: SingleChildScrollView(
+            child: Stack(
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 16 / 15,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: Image.asset('assets/unnamed.jpeg'),
           ),
-          Container(
+        ),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+            child: Container(
               decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: new BorderRadius.only(
-                    topLeft: const Radius.circular(20.0),
-                    topRight: const Radius.circular(20.0),
-                  )),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          "Poolbau",
-                          style: TextStyle(color: Colors.black, fontSize: 25.0),
+                shape: BoxShape.circle,
+                color: mainColors.main_btn,
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 25,
+                  color: mainColors.background,
+                ),
+                onPressed: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+              ),
+            )),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(0, 270, 0, 0),
+            child: Stack(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: mainColors.background,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50),
                         ),
                       ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          "Wie baue ich einen Pool?",
-                          style: TextStyle(color: Colors.black, fontSize: 14.0),
+                      child: Column(children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 40, 0, 0),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Poolbau",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: mainColors.Text,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
-                          style: TextStyle(color: Colors.black, fontSize: 14.0),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 7, 0, 0),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Schwierigkeitsgrad: mittel",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: mainColors.Text_description,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      )
-                    ]),
-              )),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemBuilder: (ctx, int) {
-                return Card(
-                  child: ListTile(
-                      title: Text('Anbieter $int'),
-                      subtitle: Text('Beschreibung')),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 30, 0, 0),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Datum",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: mainColors.Text,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 30, 0, 0),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Beschreibung",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: mainColors.Text,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(40, 7, 40, 40),
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: mainColors.Text_description,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                    )),
+              ],
+            )),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(280, 270, 0, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: mainColors.background,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.thumb_up,
+                  size: 20,
+                  color: mainColors.heart_btn,
+                ),
+                onPressed: () {},
+              ),
+            )),
+      ],
+    )));
   }
 }
